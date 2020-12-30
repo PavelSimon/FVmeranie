fad4c83127138624b9c4eb043962bcc1b888b02f
Pavel Šimon <pavel@pavel-simon.com>
2020-12-30 14:06:52 +0100

    Pridanie záložky "graf", upratanie závislostí

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+from devtools import debug
 from config import PORT
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+#from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
-import requests
+#import requests
 import time
-from dateutil.parser import parse
+#from dateutil.parser import parse
+from adafruit_ads1x15.analog_in import AnalogIn
+import adafruit_ads1x15.ads1115 as ADS
 import board
 import busio
 i2c = busio.I2C(board.SCL, board.SDA)
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
 
 app = FastAPI()
 
@@ -22,9 +23,11 @@ ads = ADS.ADS1115(i2c)
 
 chan = AnalogIn(ads, ADS.P3)
 
-print(chan.value, chan.voltage)
+# debug(chan)
 
 # Routes:
+
+
 @app.get("/")
 async def root(request: Request):
     """
@@ -35,19 +38,31 @@ async def root(request: Request):
     value2 = AnalogIn(ads, ADS.P2)
     value3 = AnalogIn(ads, ADS.P3)
 
-    meranie=[{
-        "CH0":value0.value,
-        "CH0V":value0.voltage,
-        "CH1":value1.value,
-        "CH1V":value1.voltage,
-        "CH2":value2.value,
-        "CH2V":value2.voltage,
-        "CH3":value3.value,
-        "CH3V":value3.voltage
-        }]
-    print("meranie:",meranie)
+    meranie = [{
+        "CH0": value0.value,
+        "CH0V": value0.voltage,
+        "CH1": value1.value,
+        "CH1V": value1.voltage,
+        "CH2": value2.value,
+        "CH2V": value2.voltage,
+        "CH3": value3.value,
+        "CH3V": value3.voltage
+    }]
+    # debug(meranie)
     localtime = time.asctime(time.localtime(time.time()))
-    return templates.TemplateResponse("home.html", {"request": request, "meranie": meranie,"time": localtime})
+    print("/; Čas:", localtime)
+    return templates.TemplateResponse("home.html", {"request": request, "meranie": meranie, "time": localtime})
+
+
+@app.get("/graf")
+async def graf(request: Request):
+    """
+    Zobrazí graf nameranej charakteristiky
+    """
+
+    localtime = time.asctime(time.localtime(time.time()))
+    print("Graf; Čas:", localtime)
+    return templates.TemplateResponse("graf.html", {"request": request, "time": localtime})
 
 
 # Code for running app
